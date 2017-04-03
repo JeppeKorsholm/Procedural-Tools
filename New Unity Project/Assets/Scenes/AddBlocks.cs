f3c39f3fd8ba85449cd5e5e9782e343dcccd622a
JeppeKorsholm <jeppe.korsholm@hotmail.com>
2017-04-03 19:24:33 +0200

    add tiled prefabs to a gameObject

---
+++ New Unity Project/Assets/Scenes/AddBlocks.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBlocks : MonoBehaviour {
+
+    public GameObject prefab;
+
+    public int tiling;
+    public float scale;
+    public float xVal;
+       
+
+    private void Start()
+    {
+        scale = 1f / tiling;
+        for(int i = 0; i < tiling; i++)
+        {
+            for(int j = 0; j < tiling; j++)
+            {
+                InstantiatePrefabs((float)i/(float)tiling, (float)j / (float)tiling);
+            }
+
+            
+        }
+ 
+
+
+    }
+
+    public void InstantiatePrefabs(float indexValueX, float indexValueY)
+    {
+        float xStepper, yStepper;
+
+        GameObject clone;
+        clone = Instantiate(prefab, transform.position, transform.rotation);
+        //add pillar script
+        clone.transform.parent = transform;
+        clone.transform.localScale = new Vector3(scale, scale, scale);
+
+        
+        xStepper = -1 + indexValueX * 2;
+        yStepper = -1 + indexValueY * 2;
+
+        clone.transform.position = transform.position + new Vector3((-transform.localScale.x / 2) * xStepper - clone.transform.lossyScale.x / 2, 
+                                                                    (-transform.localScale.y / 2) * yStepper - clone.transform.lossyScale.y / 2, 
+                                                                      transform.localScale.z / 2 + clone.transform.lossyScale.z /2);
+
+      
+        clone.GetComponent<Renderer>().material.color = new Color(Random.Range(0, 1f), 0, 0);
+    }
+
+
+  
+}
